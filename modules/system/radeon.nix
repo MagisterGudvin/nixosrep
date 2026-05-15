8fc08b9549d8c1cfe367866994c7a136f7901d55
Magister Gudvin <83319102+MagisterGudvin@users.noreply.github.com>
2026-05-15 09:26:56 +0300

    Update radeon.nix

--- modules/system/radeon.nix
+++ modules/system/radeon.nix
@@ -1,17 +1,24 @@
 { ... }: {
   flake.nixosModules.radeon = { pkgs, ... }: {
-    services.xserver.videoDrivers = [ "amdgpu" ];
+    services.xserver.videoDrivers = [ "modesetting" ];
 
     hardware.graphics = {
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        rocmPackages.clr.icd
-        amdvlk
+        mesa
+        libva-vdpau-driver
+        libvdpau-va-gl
       ];
-      extraPackages32 = with pkgs.driversi686Linux; [
-        amdvlk
+      extraPackages32 = with pkgs; [
+        driversi686Linux.mesa
       ];
+    };
+
+    environment.variables = {
+      AMD_VULKAN_ICD = "RADV";
+      LIBVA_DRIVER_NAME = "radeonsi";
+      VDPAU_DRIVER = "radeonsi";
     };
   };
 }
