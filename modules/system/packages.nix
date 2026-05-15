bfbc2a262cf577efddf98f4603a93676eac42d03
Magister Gudvin <83319102+MagisterGudvin@users.noreply.github.com>
2026-05-15 09:38:17 +0300

    Update packages.nix

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -1,6 +1,26 @@
 { ... }: {
   flake.nixosModules.packages = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
+    wget
+    git
+    vim
+    curl
+    unzip
+    zip
+    btop
+    python3
+    ffmpeg
+    pciutils
+    docker-compose
+    wl-clipboard
+    brightnessctl
+    playerctl
+    xdg-utils
+    tree
+    ntfs3g
+    hyprpolkitagent
+    visidata
+    jdk21
     ];
   };
 }
